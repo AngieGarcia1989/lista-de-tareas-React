8ecda568a96ebf7c5e7ddf569b59ec7787624d06
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-15 21:17:37 -0500

    [setup] eslint, husky , CI

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,35 +1,38 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+/* eslint-disable import/no-unresolved */
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        home: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
+  entry: {
+    home: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
 
-            },
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title: 'lista de tareas',
-            hash: true,
-            template: path.resolve(__dirname, 'public/index.html')
-        }),
-        new webpack.DllReferencePlugin({
-            manifest: require('./modules-manifest.json'),
-            context: path.resolve(__dirname, 'src'),
-          }),
-    ]
-}
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'lista de tareas',
+      hash: true,
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require('./modules-manifest.json'),
+      context: path.resolve(__dirname, 'src'),
+    }),
+  ],
+};

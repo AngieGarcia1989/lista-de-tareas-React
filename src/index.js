9372316711b74ad200a1d289a386b7d6a16fd767
Angie garcia <62357966+AngieGarcia1989@users.noreply.github.com>
2021-02-16 12:39:42 -0500

    [setup] CI, husky, integracion continua

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import {render} from 'react-dom'
-import App from './App'
+import React from 'react';
+import { render } from 'react-dom';
 
-render(<App/>, document.getElementById('root'))
+import App from './App';
+
+render(<App />, document.getElementById('root'));
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,39 +1,40 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        home: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
+  entry: {
+    home: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
 
-            },
-        ]
-    },
-    devServer: {
-        host: '0.0.0.0',
-        port: 3000,
-        disableHostCheck: false,
-        open: true,
-        hot: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HTMLWebpackPlugin({
-            title: 'Lista de tareas',
-            hash: false,
-            template: path.resolve(__dirname, 'public/index.html')
-        })
-    ]
-}
+      },
+    ],
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    disableHostCheck: false,
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Lista de tareas',
+      hash: false,
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+};
